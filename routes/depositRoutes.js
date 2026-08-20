041d6b13ee4706a767b4596e574535732c64e1f0
ManzzyGacor <manzz9293@gmail.com>
2026-08-20 15:35:40 +0700

    Add files via upload

--- routes/depositRoutes.js
+++ routes/depositRoutes.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const depositController = require('../controllers/depositController');
+const jwt = require('jsonwebtoken');
 
-router.get('/create', depositController.createDeposit);
-router.get('/check', depositController.checkDeposit);
-router.get('/cancel', depositController.cancelDeposit);
+// Middleware Verifikasi Token
+const verifyToken = (req, res, next) => {
+    const token = req.header('Authorization');
+    if (!token) return res.status(401).json({ success: false, message: 'Akses ditolak.' });
+    try {
+        const decoded = jwt.verify(token.replace('Bearer ', ''), process.env.SESSION_SECRET);
+        req.user = decoded.user;
+        next();
+    } catch (err) {
+        res.status(401).json({ success: false, message: 'Token tidak valid' });
+    }
+};
+
+router.get('/create', verifyToken, depositController.createDeposit);
+router.get('/check', verifyToken, depositController.checkDeposit);
+router.get('/cancel', verifyToken, depositController.cancelDeposit);
 
 module.exports = router;
