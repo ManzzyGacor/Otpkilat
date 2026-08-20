0c9d2625ca7fe0adf1d092b817470cf2da8b9642
ManzzyGacor <manzz9293@gmail.com>
2026-08-20 10:33:21 +0700

    Delete src directory

--- src/middleware/admin.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res, next) => {
-  if (req.session.role !== 'admin') {
-    if (req.xhr || (req.headers.accept && req.headers.accept.includes('application/json'))) {
-      return res.status(403).json({ success: false, error: { message: 'Akses ditolak. Fitur ini hanya untuk Admin.' } });
-    }
-    return res.status(403).send('Akses Ditolak: Anda tidak memiliki izin untuk melihat halaman ini.');
-  }
-  next();
-};
